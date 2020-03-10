c5ed33a0a8eb86d3cb96d6b6739c1a2103d1a409
Bartosz <bartptak@gmail.com>
2020-03-10 19:49:35 +0100

    Add arrowhead to graph

--- graph_visualization/creator.py
+++ graph_visualization/creator.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import networkx as nx
 
-G = nx.Graph()
+G = nx.DiGraph()
 
 # nodes
 G.add_node('AS off')
@@ -9,7 +9,7 @@ G.add_node('AS manual')
 G.add_node('AS ready')
 G.add_node('AS driving')
 G.add_node('AS finished')
-G.add_node('AS emergancy')
+G.add_node('AS emergancy', weight='weight')
 
 # edges
 G.add_edge('AS off', 'AS manual')
@@ -23,15 +23,23 @@ G.add_edge('AS finished', 'AS off')
 G.add_edge('AS ready', 'AS emergancy')
 G.add_edge('AS driving', 'AS emergancy')
 G.add_edge('AS finished', 'AS emergancy')
+G.add_edge('AS emergancy', 'AS off')
 
+pos = nx.spring_layout(G)
 
 OPTIONS = {
     'node_color': 'skyblue',
-    'node_size': 500,
-    'width': 1,
+    'node_size':5000,
+    'width': 2,
     'with_labels': True
 }
 
-plt.figure(figsize=(10, 10))
+plt.figure(figsize=(8,8))
+
 nx.draw(G, **OPTIONS)
+# nx.draw_networkx_edge_labels(G,pos,edge_labels={
+#     ('AS off', 'AS manual'):'AB',
+#     ('AS manual', 'AS off'): 'BA'
+
+# },font_color='red')
 plt.savefig("graph.png")
